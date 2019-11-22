5df14d9b0686f50ab7001aa165eeba5cc3be8fc9
Gustav0187 <36207633+Gustav0187@users.noreply.github.com>
2019-11-22 11:53:45 -0700

    Add files via upload
    yiii

---
+++ Models.cs
@@ -0,0 +1,20 @@
+
+
+namespace src
+{
+
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string genero { get; set; }
+        public int precio { get; set; }
+    }
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
